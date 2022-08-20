fb7ab024b978c1c0ed1da41c5fd434e8371b2079
Julien Galletta <humbkr@gmail.com>
2022-08-20 11:01:15 +0200

    feature: improve users endpoints (#26)

--- server/internal/interfaces/graph/model/user.go
+++ server/internal/interfaces/graph/model/user.go
@@ -2,7 +2,7 @@ package model
 
 type User struct {
 	ID        int       `json:"id"`
-	Name      string    `json:"name"`
+	Name      *string   `json:"name"`
 	Email     *string   `json:"email"`
 	Password  *string   `json:"password"`
 	Data      *string   `json:"data"`
