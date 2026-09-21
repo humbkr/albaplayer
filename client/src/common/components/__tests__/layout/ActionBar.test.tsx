e551077ce08f370677693242ed23fc3610002a79
Julien Galletta <humbkr@gmail.com>
2026-09-21 19:41:50 +0200

    feat: app / library update notifications

--- client/src/common/components/__tests__/layout/ActionBar.test.tsx
+++ client/src/common/components/__tests__/layout/ActionBar.test.tsx
@@ -7,11 +7,11 @@ import { renderWithProviders } from 'common/utils/testing/test-utils'
 vi.mock('modules/browser/components/SearchBar', () => ({
   default: () => <div data-testid="search-bar" />,
 }))
-vi.mock('common/components/layout/LibraryUpdateButton', () => ({
+vi.mock('modules/library/components/LibraryUpdateButton', () => ({
   default: () => <div data-testid="library-update-button" />,
 }))
-vi.mock('common/components/layout/UpdateButton', () => ({
-  default: () => <div data-testid="update-button" />,
+vi.mock('modules/settings/components/AppUpdateButton', () => ({
+  default: () => <div data-testid="app-update-button" />,
 }))
 vi.mock('modules/user/components/UserActionsMenu', () => ({
   default: () => <div data-testid="user-action-menu" />,
